433849be2b4b8cd6b5a054e9605cd3bb297aa59d
Halenursozer <101124940+Halenursozer@users.noreply.github.com>
2022-03-25 16:11:52 +0300

    Add files via upload

---
+++ server__sql/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿
+namespace server__sql
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtHost = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.btnstart = new System.Windows.Forms.Button();
+            this.btnstop = new System.Windows.Forms.Button();
+            this.txtStatus = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "IP:";
+            // 
+            // txtHost
+            // 
+            this.txtHost.Location = new System.Drawing.Point(84, 51);
+            this.txtHost.Name = "txtHost";
+            this.txtHost.Size = new System.Drawing.Size(117, 20);
+            this.txtHost.TabIndex = 1;
+            this.txtHost.Text = "127.0.0.1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(239, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "PORT:";
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(285, 51);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(117, 20);
+            this.txtPort.TabIndex = 3;
+            this.txtPort.Text = "3000";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(473, 51);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(191, 23);
+            this.progressBar1.TabIndex = 4;
+            // 
+            // btnstart
+            // 
+            this.btnstart.Location = new System.Drawing.Point(473, 110);
+            this.btnstart.Name = "btnstart";
+            this.btnstart.Size = new System.Drawing.Size(75, 23);
+            this.btnstart.TabIndex = 5;
+            this.btnstart.Text = "start";
+            this.btnstart.UseVisualStyleBackColor = true;
+            this.btnstart.Click += new System.EventHandler(this.btnstart_Click);
+            // 
+            // btnstop
+            // 
+            this.btnstop.Location = new System.Drawing.Point(589, 110);
+            this.btnstop.Name = "btnstop";
+            this.btnstop.Size = new System.Drawing.Size(75, 23);
+            this.btnstop.TabIndex = 6;
+            this.btnstop.Text = "stop";
+            this.btnstop.UseVisualStyleBackColor = true;
+            this.btnstop.Click += new System.EventHandler(this.btnstop_Click);
+            // 
+            // txtStatus
+            // 
+            this.txtStatus.Location = new System.Drawing.Point(46, 157);
+            this.txtStatus.Multiline = true;
+            this.txtStatus.Name = "txtStatus";
+            this.txtStatus.Size = new System.Drawing.Size(356, 152);
+            this.txtStatus.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtStatus);
+            this.Controls.Add(this.btnstop);
+            this.Controls.Add(this.btnstart);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtHost);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtHost;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button btnstart;
+        private System.Windows.Forms.Button btnstop;
+        private System.Windows.Forms.TextBox txtStatus;
+    }
+}
+
---
+++ server__sql/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using Newtonsoft.Json;
+
+namespace server__sql
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection Connection = new SqlConnection();
+        string ConnectionAddress = "Server=.;Database=Registers;User Id=sa;Password=123456;connection timeout=30;";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SimpleTCP.SimpleTcpServer server;
+
+        public class Alarms
+        {
+            public string serialnumber { get; set; }
+            public int P1 { get; set; }
+            public int P2 { get; set; }
+            public int P3 { get; set; }
+            public int P4 { get; set; }
+            public int P5 { get; set; }
+            public int P6 { get; set; }
+            public int P7 { get; set; }
+            public int P8 { get; set; }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            server = new SimpleTCP.SimpleTcpServer();
+            server.Delimiter = 0x13;//enter
+            server.StringEncoder = Encoding.UTF8;
+            server.DataReceived += Server_DataReceived;
+            
+        }
+       
+        private void Server_DataReceived(object sender, SimpleTCP.Message e)
+        {
+            txtStatus.Invoke((MethodInvoker)delegate ()
+            {
+                txtStatus.Clear();
+                txtStatus.Text += e.MessageString;
+                e.ReplyLine(string.Format("You said: {0}", e.MessageString));
+                string jsontext = txtStatus.Text;
+                Alarms alarms = JsonConvert.DeserializeObject<Alarms>(jsontext);
+                //textBox1.Text = (alarmlar.P1).ToString();
+                getvalue((alarms.P1).ToString(), (alarms.P2).ToString(), (alarms.P3).ToString(), (alarms.P4).ToString(), (alarms.P5).ToString(), (alarms.P6).ToString(), (alarms.P7).ToString(), (alarms.P8).ToString(), (alarms.serialnumber).ToString());
+            });
+        }
+
+        private void btnstart_Click(object sender, EventArgs e)
+        {
+            //Start server host
+            progressBar1.Value = 100;
+            System.Net.IPAddress ip = System.Net.IPAddress.Parse(txtHost.Text);
+            server.Start(ip, Convert.ToInt32(txtPort.Text));
+        }
+
+        private void btnstop_Click(object sender, EventArgs e)
+        {
+            if (server.IsStarted)
+                server.Stop();
+            progressBar1.Value = 0;
+
+        }
+        private void getvalue(string P1, string P2, string P3, string P4, string P5, string P6, string P7, string P8, string serialnumber)
+        {
+            Connection.ConnectionString = ConnectionAddress;
+            Connection.Open();
+            string register = "INSERT INTO stateofports(Date,serialnumber,P1,P2,P3,P4,P5,P6,P7,P8) VALUES (@Date,@serialnumber,@P1,@P2,@P3,@P4,@P5,@P6,@P7,@P8)";
+            SqlCommand komut = new SqlCommand(register, Connection);
+            komut.Parameters.AddWithValue("@Date", DateTime.Now.ToString("MM/dd/yyyy HH:mm"));
+            komut.Parameters.AddWithValue("serialnumber", serialnumber);
+            komut.Parameters.AddWithValue("@P1", P1);
+            komut.Parameters.AddWithValue("@P2", P2);
+            komut.Parameters.AddWithValue("@P3", P3);
+            komut.Parameters.AddWithValue("@P4", P4);
+            komut.Parameters.AddWithValue("@P5", P5);
+            komut.Parameters.AddWithValue("@P6", P6);
+            komut.Parameters.AddWithValue("@P7", P7);
+            komut.Parameters.AddWithValue("@P8", P8);
+            komut.ExecuteNonQuery();
+            Connection.Close();
+        }
+    }
+}
